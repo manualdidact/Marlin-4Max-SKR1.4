63ab05b7aa60203d81c036c1710773c8afc0d424
Chris Robison <chris@chrisrobison.org>
2020-06-07 12:02:06 -0500

    Initial SKR settings changes

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -420,7 +420,10 @@
  * Multiple extruders can be assigned to the same pin in which case
  * the fan will turn on when any selected extruder is above the threshold.
  */
-#define E0_AUTO_FAN_PIN 44
+//From 4Max settings:
+//#define E0_AUTO_FAN_PIN 44
+//Mod for SKR
+#define E0_AUTO_FAN_PIN P2_04
 #define E1_AUTO_FAN_PIN -1
 #define E2_AUTO_FAN_PIN -1
 #define E3_AUTO_FAN_PIN -1
